76867d9eb2a62bea51cc2248b6a34bf8f50ba678
Edmundo Pichardo <clemente.pichardo@theoreminc.net>
2016-04-05 09:53:35 -0400

    Ahora vamos a empezar a agregar User Validations: username must be present and unique, email  same, and validate email format using regex

--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,6 @@ ActiveRecord::Schema.define(version: 20160321203117) do
     t.integer  "user_id"
   end
 
-
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "email"
